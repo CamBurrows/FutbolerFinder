4b52a138ba8708c9f599751cb643f493d29f792b
Cameron Burrows <cburrows@Camerons-MacBook-Pro.local>
2018-04-03 00:10:32 -0400

    finished form

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "tables.html"));
+    res.sendFile(path.join(__dirname, "survey.html"));
 });
 
 app.post("/api/tables", function (req, res) {
