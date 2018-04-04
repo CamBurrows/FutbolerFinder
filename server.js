db4c6bfc9cde0aae4726aff41190c44f20e8fc17
Cameron Burrows <cburrows@wifi_stu-10-38-82-232.uncc.edu>
2018-04-04 10:04:27 -0400

    further progression on assignment

--- server.js
+++ server.js
@@ -5,18 +5,90 @@ var path = require("path");
 var PORT = process.env.PORT || 3000;
 var app = express();
 
+app.use(express.static("app/public"));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+var individualArr = [];
+var teamArr = [];
+var NewProfile;
+
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
 });
 
 app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
 });
 
-app.post("/api/tables", function (req, res) {
-    var reserve = req.params.reservations;
-    return res.json(reservations)
+app.get("/api/teams", function (req, res) {
+    return res.json(teamArr);
+});
+app.get("/api/individuals", function (req, res) {
+    return res.json(individualArr);
+});
+
+app.post("/api/new", function (req, res) {
+    NewProfile = req.body;
+    
+    if (NewProfile.searchingFor === "team"){
+        individualArr.push(NewProfile)
+        var differenceArray = []
+
+        for (i = 0; i < teamArr.length; i++){
+            var compareValues = teamArr[i].questions;
+            var compareAgainst = NewProfile.questions;
+            var totalDifference;
+            console.log("compare values: " + compareValues);
+
+            for (z = 0; z < compareValues.length; z++){
+                var difference = Math.abs(parseInt(compareValues[z]) - parseInt(compareAgainst[z]));
+                totalDifference += difference
+            }
+
+            console.log("total difference : " + totalDifference)
+            differenceArray.push(totalDifference)
+            console.log("difference array: " + differenceArray)
+        }
+
+        var lowValue = Math.min(...differenceArray);
+        var match = differenceArray.indexOf(lowValue);
+
+        console.log("Low Value: " + lowValue);
+        console.log("match: " + match)
+        
+    }
+    else {
+        teamArr.push(NewProfile)
+        var differenceArray = []
+
+        for (i = 0; i < individualArr.length; i++){
+            var compareValues = individualArr[i].questions;
+            var compareAgainst = NewProfile.questions;
+            var totalDifference;
+            console.log("compareValues: " + compareValues);
+
+            for (z = 0; z < compareValues.length; z++){
+                var difference = Math.abs(parseInt(compareValues[z]) - parseInt(compareAgainst[z]));
+                totalDifference += difference
+            }
+
+            console.log("total difference : " + totalDifference)
+            differenceArray.push(totalDifference)
+            console.log("difference array: " + differenceArray)
+        }
+
+        var lowValue = Math.min(...differenceArray);
+        var match = differenceArray.indexOf(lowValue);
+
+        console.log("Low Value: " + lowValue);
+        console.log("match: " + match)
+    }
+    console.log(NewProfile);
+    res.json(NewProfile);
 })
+
+app.listen(PORT, function () {
+    console.log("Server listening on http://localhost:" + PORT);
+});
